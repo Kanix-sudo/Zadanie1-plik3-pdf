4dcca2bf2afdd14a6fda50d657e3a1c27b917846
Wojciech Szyjka <wojtekszyjka@outlook.com>
2020-10-23 17:02:08 +0200

    Add project files.

---
+++ Zadanie1-plik3 pdf/Zadanie1-plik3 pdf.cpp
@@ -0,0 +1,22 @@
+#include <math.h>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+const float Pi = 3.14159265359f;
+const float E = 2.718281828459f;
+const float Z = 1.6180339887498948482f;
+
+
+
+int main()
+{
+	cout << "Witaj" << endl;
+
+	cout << "PI = " << fixed << setprecision(4) << Pi;
+	cout << "   e =  " << E;
+	cout << "   Zloty srodek = " << Z << endl;
+	system("pause");
+
+}
